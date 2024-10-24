ba064d2be65e07b979a1d3073f61d53548ebdad1
Sandeep <24je0686@iitism.ac.in>
2024-10-24 09:56:58 +0530

    inverse

--- calcu.c
+++ calcu.c
@@ -27,6 +27,10 @@ int cube (int i){
 return (i*i*i);
 }
 
+float inverse(int i){
+return (1/(float)i);
+}
+
 int main() {
     int a, b;
     char operation;
@@ -81,7 +85,7 @@ int main() {
             case 'i':
                 printf("Enter an integer: ");
                 scanf("%d", &a);
-                printf("Will be implemented soon...\n");
+                printf("The result of 1/ %d is %.2f \n",a,inverse(a));
                 break;
             default:
                 printf("Invalid operation. Please try again.\n");
